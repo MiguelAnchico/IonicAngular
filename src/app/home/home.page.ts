fa6b2dd33e93d949e49c1e81c69b13576059b962
unknown <Administrador@T124904.uao.edu.co>
2023-09-30 16:26:37 -0500

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,66 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  Nombre: string = "";
+  Mensaje: string = "";
+  Relacion: string = "";
+  Materias: string = "";
 
-  constructor() {}
+  mostrar: boolean = false;
 
+
+  items = [
+    {titulo: "Ingeniero", descripcion: "Electronico"},
+    {titulo: "Medico", descripcion: "Cirujano"},
+    {titulo: "Abogado", descripcion: "Penalista"},
+  ]
+
+  constructor(public alertController:AlertController, public toastController: ToastController) {}
+
+  getNombre(){
+    this.Mensaje = "Bienvenido " + this.Nombre + " tu eres mi " + this.Relacion + " y estudias " + this.Materias + "!"
+  }
+
+  getLimpiar() {
+    this.Mensaje="";
+    this.presentToast();
+  }
+
+  setMostrar() {
+    this.mostrar = true;
+  }
+
+  async presentAlert(){
+    const alert = await this.alertController.create({
+      header: "info",
+      subHeader: "Estudiante",
+      message: "Mensaje a guardar",
+      buttons:[
+        {
+          text: "Cancelar",
+          role:"cancel",
+          handler: (dato) => this.getLimpiar(), 
+        },
+        {
+          text: "Aceptar",
+          role:"ok",
+          handler: (dato) => this.getNombre(),
+        }
+      ]
+    })
+
+    
+    await alert.present();
+    await alert.onDidDismiss();
+  }
+  
+  async presentToast(){
+    const toast = await this.toastController.create({
+      header: "Notificacion",
+      message: "El mensaje fue borrado",
+      duration: 2000,
+    })
+
+    await toast.present();
+  }
 }
